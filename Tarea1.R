08558276858e5368751125470b2827f26d33614d
SAeryus <raforafo1@gmail.com>
2016-04-12 20:35:59 -0500

    Segundo commit

--- Tarea1.R
+++ Tarea1.R
@@ -2,4 +2,7 @@ print('holii')
 setwd("C:/Users/Rafael/Archivos/Universidad/Ciencias/Fisica/TecnicasBazo/Tarea1/")
 getwd()
 data<-read.table("NuAstro_4yr_IceCube_Data.txt")
-data[1:6,]
+print(data[30:36,])
+print("Se eliminan los datos incompletos")
+data<-data[complete.cases(data),]  ##Eliminar los datos incompletos
+data[30:36,]
