6394b18a4acc866ca5c48fc60706406f1c21db42
SAeryus <raforafo1@gmail.com>
2016-04-12 20:53:17 -0500

    tercer commit

--- Tarea1.R
+++ Tarea1.R
@@ -2,7 +2,11 @@ print('holii')
 setwd("C:/Users/Rafael/Archivos/Universidad/Ciencias/Fisica/TecnicasBazo/Tarea1/")
 getwd()
 data<-read.table("NuAstro_4yr_IceCube_Data.txt")
+print("Hay errores en la fuente")
 print(data[30:36,])
 print("Se eliminan los datos incompletos")
 data<-data[complete.cases(data),]  ##Eliminar los datos incompletos
 data[30:36,]
+y<-summary(data$V9)
+str(y)
+names(y)[y>1]
